5868eb5c552c867c922c93cc6d1784a6c5562936
Hélain Schoonjans <helain.schoonjans@eurekanetwork.org>
2019-02-01 10:25:08 +0100

    Started the guide.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Grails upload file guide
+
+http://guides.grails.org/grails-upload-file/guide/index.html
--- initial/grails-app/domain/example/grails/Restaurant.groovy
+++ initial/grails-app/domain/example/grails/Restaurant.groovy
@@ -2,10 +2,15 @@ package example.grails
 
 class Restaurant {
     String name
+    byte[] featuredImageBytes
+    String featuredImageContentType
 
     static constraints = {
+        featuredImageBytes nullable: true
+        featuredImageContentType nullable: true
     }
 
     static mapping = {
+        featuredImageBytes column: 'featured_image_bytes', sqlType: 'longblob'
     }
 }
